5f51de1235a536b92c8919d5c7233bbb5f540b74
vikram17dp <vikramdp505@gmail.com>
2024-09-21 23:45:00 +0530

    signup api controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,56 @@
-export const signup = async(req,res,next)=>{
-    res.send("signup")
-}
+import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
-export const login = async(req,res,next)=>{
-    res.send("login")
-}
-export const logout = async(req,res,next)=>{
-    res.send("logout")
-}
+export const signup = async (req, res, next) => {
+  try {
+    const { name, email, password, username } = req.body;
+    if(!name|| !username || !email|| !password){
+        return res.status(400).json({message:"All Fields are required"})
+    }
+    const existinguserName = await User.findOne({ username });
+    if (existinguserName) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+    const existingemail= await User.findOne({email});
+    if(existingemail){
+        return res.status(400).json({message:"Email laready exists"})
+    }
+    if(password.length < 4){
+        return res.status(400).json({message:"Password must be conatin at least 4 characters"})
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedpassword = await bcrypt.hash(password,salt);
+    const user = new User({
+        name,
+        email,
+        username,
+        password:hashedpassword
+    })
+
+    await user.save();
+    const token = jwt.sign({userId:user._id},process.env.JWT_SECRET,{expiresIn:"3d"});
+
+    res.cookie("jwt-linkdein",token,{
+        httpOnly:true,
+        maxAge:3*24*60*1000,
+        sameSite:"strict",
+        secure:process.env.NODE_ENV === "production"
+    })
+res.status(201).json({message:"User registred succesfully!"})
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+    
+  }
+};
+
+export const login = async (req, res, next) => {
+    
+
+
+};
+export const logout = async (req, res, next) => {
+  res.send("logout");
+};
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
         unique:true
     },
     email:{
-        type:email,
+        type:String,
         required:true,
         unique:true
     },
