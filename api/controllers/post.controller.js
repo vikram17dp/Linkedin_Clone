1656b6ab8f78e5c3fa934ef1de9b8ba5bf82938f
vikram17dp <vikramdp505@gmail.com>
2024-09-22 22:07:04 +0530

    adding the getFeddPosts controller routes

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,2 +1,16 @@
+import Post from "../models/post.model.js";
 
+
+export const getFeedPosts = async(req,res)=>{
+    try {
+        const posts = await Post.find({author:{$in:req.user.connections}})
+        .populate("author","name username profilePicture headline")
+        .populate("comments:user","name profilePicture")
+        .sort({createdAt:-1})
+        res.status(200).json(posts)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message:"Internal Sever error"})
+    }
+}
 
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,6 +1,9 @@
 import express from 'express'
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { getFeedPosts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
+router.get('/',protectRoute,getFeedPosts)
 
 export default router;
