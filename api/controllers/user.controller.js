524d6d9bf6c3fcf8791da114d45ff67393d7581b
vikram17dp <vikramdp505@gmail.com>
2024-09-22 14:25:28 +0530

    updating the user controller file

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudniary.js";
 import User from "../models/user.model.js";
 
 export const getSuggestedConnections = async (req, res) => {
@@ -18,45 +19,59 @@ export const getSuggestedConnections = async (req, res) => {
   }
 };
 
-
-export const getPublicProfile = async (req,res)=>{
-    try {
-        const user = await User.findOne({username:req.params.username}).select("-password");
-        if(!user){
-            res.status(404).json({message:"Usre not found"})
-        }
-        res.json(user);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message:"Internal server error!"});
+export const getPublicProfile = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username }).select(
+      "-password"
+    );
+    if (!user) {
+      res.status(404).json({ message: "Usre not found" });
     }
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error!" });
+  }
 };
 
-export const getprofileupdate = async (req,res)=>{
-    try {
-        const allowedFileds = [
-            "name",
-            "username",
-            "profilepicture",
-            "headline",
-            "about",
-            "location",
-            "bannerImg",
-            "skills",
-            "experience",
-            "education"
-        ];
-        const updatedData = {};
-        for(const field of allowedFileds){
-            if(req.body[field]){
-                updatedData[field] = req.body[field];
-            }
-        }
-
-        const user = await User.findByIdAndUpdate(req.user._id,{$set:updatedData},{new:true}).select("-password");
-        res.json(user);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message:"Internal server error!"});
+export const getprofileupdate = async (req, res) => {
+  try {
+    const allowedFileds = [
+      "name",
+      "username",
+      "profilepicture",
+      "headline",
+      "about",
+      "location",
+      "bannerImg",
+      "skills",
+      "experience",
+      "education",
+    ];
+    const updatedData = {};
+    for (const field of allowedFileds) {
+      if (req.body[field]) {
+        updatedData[field] = req.body[field];
+      }
     }
-}
+
+    if(req.body.profilepicture){
+      const result = await cloudinary.uploader.upload(req.body.profilepicture);
+      updatedData.profilepicture = result.secure_url;
+    }
+    if(req.body.bannerImg){
+      const result = await cloudinary.uploader.upload(req.body.bannerImg);
+      updatedData.bannerImg = result.secure_url;
+    }
+
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { $set: updatedData },
+      { new: true }
+    ).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error!" });
+  }
+};
