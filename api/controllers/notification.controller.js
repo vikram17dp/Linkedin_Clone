602968599c4fc31813d65c86983e2228005247f1
vikram17dp <vikramdp505@gmail.com>
2024-09-23 00:20:52 +0530

    adding the getUserNotifications to the controller

--- api/controllers/notification.controller.js
+++ api/controllers/notification.controller.js
@@ -1,11 +1,21 @@
+import Notification from "../models/notification.model.js"
 
 export const getUserNotifications = async(req,res)=>{
-
+    try {
+        const notifications = await Notification.find({recipent:req.user._id})
+        .sort({createdAt:-1})
+        .populate("relatedUser","name username profilePicture")
+        .populate("relatedPost","content image")
+        res.status(200).json(notifications)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message:"Internal server error!"})
+    }
 }
 export const markNotificationAsread = async(req,res)=>{
 
 }
 
 export const deleteNotification = async(req,res)=>{
-    
+
 }
