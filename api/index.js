0f78ab4db9035103544ff41ecb32f648071952f4
vikram17dp <vikramdp505@gmail.com>
2024-09-23 12:58:33 +0530

    conection route and schema

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth.route.js'
 import userRoutes from './routes/user.route.js'
 import postRoutes from './routes/post.route.js'
 import notificationRoutes from './routes/notification.routes.js'
+import connectionRoute from './routes/connections.route.js'
 
 import cookieParser from "cookie-parser";
 
@@ -22,6 +23,7 @@ app.use('/api/v1/auth',authRoutes)
 app.use('/api/v1/users',userRoutes)
 app.use('/api/v1/posts',postRoutes)
 app.use('/api/v1/notifications',notificationRoutes)
+app.use('/api/v1/connection',connectionRoute)
 
 app.listen(PORT, () => {
   console.log(`The server is listening on ${PORT}`);
---
+++ api/models/connectionRequest.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const connectionRequestSchema = new mongoose.Schema({
+    sender:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    recipient:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    status:{
+        type:String,
+        enum:["pending","accepted","rejected"],
+        default:"pending"
+    }
+
+},{timestamps:true});
+
+export const Connection = mongoose.model("Connection",connectionRequestSchema);
+
+export default connectionRequestSchema;
