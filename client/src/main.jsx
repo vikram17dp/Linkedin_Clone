25ed812e7ace5d5c3e1d3f502405ca3dd363dee1
vikram17dp <vikramdp505@gmail.com>
2024-09-24 12:54:52 +0530

    instaltions of the tanstack/react-query

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,23 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+    <QueryClientProvider>
+
       <App />
+    </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
