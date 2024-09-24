572b9e61ecf09cb042ca33d4b75b846425822a39
vikram17dp <vikramdp505@gmail.com>
2024-09-24 11:31:02 +0530

    rejectConnectionRequest to the user

--- api/controllers/connection.controller.js
+++ api/controllers/connection.controller.js
@@ -1,77 +1,119 @@
 import connectionRequestSchema from "../models/connectionRequest.model.js";
 import User from "../models/user.model.js";
-import Notification from "../models/notification.model.js"
+import Notification from "../models/notification.model.js";
 import { sendConnectionAcceptedEmail } from "../Emails/emailHandlers.js";
 
-
-export const sendConnectionRequest = async (req,res)=>{
-    try {
-        const {userId } = req.params;
-        const senderId = req.user._id;
-        if(senderId.toString() === userId){
-            return res.status(400).json({message:"You can't send a request to youerself"})
-        }
-        if( req.user.connections.includes(userId)){
-            return res.status(400).json({message:"You are already connected"})
-        }
-        const existingRequest = await connectionRequestSchema.findOne({
-            sender:senderId,
-            recipent:userId,
-            status:"pending"
-        })
-        if(existingRequest){
-            return res.status(400).json({message:"A connection request already exists"})
-        }
-        const newRequest = new connectionRequestSchema({
-            sender:senderId,
-            recipent:userId
-        })
-        await newRequest.save();
-        res.status(201).json({message:"Connection request sent successfully"})
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message:"Server error"})
+export const sendConnectionRequest = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const senderId = req.user._id;
+    if (senderId.toString() === userId) {
+      return res
+        .status(400)
+        .json({ message: "You can't send a request to youerself" });
     }
-}
-
-export const acceptConnectionRequest = async(req,res)=>{
-    try {
-        const {requestId} = req.params;
-        const userId = req.user._id;
-        const request = await connectionRequestSchema.findById(requestId)
-        .populate("sender","name email username")
-        .populate("recipient","name username")
-        if (!request) {
-			return res.status(404).json({ message: "Connection request not found" });
-		}
-        if (request.recipient._id.toString() !== userId.toString()) {
-			return res.status(403).json({ message: "Not authorized to accept this request" });
-		}
-        if (request.status !== "pending") {
-			return res.status(400).json({ message: "This request has already been processed" });
-		}
-        request.status = "pending";
-        await request.save();
-        await User.findByIdAndUpdate(request.sender._id,{$addToSet:{connections:userId}})
-        await User.findByIdAndUpdate(userId,{$addToSet:{connections:request.sender._id}});
-        const notification = new Notification({
-            recipient:request.sender._id,
-            type:"connectionAccepted",
-            relatedUser:userId
-        })
-        await notification.save();
-        const senderEmail = request.sender.email;
-        const senderName = request.sender.name;
-        const recipientName = request.recipent.name;
-        const profileUrl = process.env.CLIENT_URL + "/profile/" + request.recipient.username;
-        try {
-            await sendConnectionAcceptedEmail(senderEmail, senderName, recipientName, profileUrl);
-        } catch (error) {
-            console.error(error)
-        }
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message:"Server Error"})
+    if (req.user.connections.includes(userId)) {
+      return res.status(400).json({ message: "You are already connected" });
     }
-}
+    const existingRequest = await connectionRequestSchema.findOne({
+      sender: senderId,
+      recipent: userId,
+      status: "pending",
+    });
+    if (existingRequest) {
+      return res
+        .status(400)
+        .json({ message: "A connection request already exists" });
+    }
+    const newRequest = new connectionRequestSchema({
+      sender: senderId,
+      recipent: userId,
+    });
+    await newRequest.save();
+    res.status(201).json({ message: "Connection request sent successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const acceptConnectionRequest = async (req, res) => {
+  try {
+    const { requestId } = req.params;
+    const userId = req.user._id;
+    const request = await connectionRequestSchema
+      .findById(requestId)
+      .populate("sender", "name email username")
+      .populate("recipient", "name username");
+    if (!request) {
+      return res.status(404).json({ message: "Connection request not found" });
+    }
+    if (request.recipient._id.toString() !== userId.toString()) {
+      return res
+        .status(403)
+        .json({ message: "Not authorized to accept this request" });
+    }
+    if (request.status !== "pending") {
+      return res
+        .status(400)
+        .json({ message: "This request has already been processed" });
+    }
+    request.status = "pending";
+    await request.save();
+    await User.findByIdAndUpdate(request.sender._id, {
+      $addToSet: { connections: userId },
+    });
+    await User.findByIdAndUpdate(userId, {
+      $addToSet: { connections: request.sender._id },
+    });
+    const notification = new Notification({
+      recipient: request.sender._id,
+      type: "connectionAccepted",
+      relatedUser: userId,
+    });
+    await notification.save();
+    const senderEmail = request.sender.email;
+    const senderName = request.sender.name;
+    const recipientName = request.recipent.name;
+    const profileUrl =
+      process.env.CLIENT_URL + "/profile/" + request.recipient.username;
+    try {
+      await sendConnectionAcceptedEmail(
+        senderEmail,
+        senderName,
+        recipientName,
+        profileUrl
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+export const rejectConnectionRequest = async (req, res) => {
+  try {
+    const { requestId } = req.params;
+    const userId = req.user._id;
+
+    const request = await connectionRequestSchema.findById(requestId);
+    if (request.recipient.toString() !== userId.toString()) {
+      return res
+        .status(403)
+        .json({ message: "Not authorized to reject this request" });
+    }
+
+    if (request.status !== "pending") {
+      return res
+        .status(400)
+        .json({ message: "This request has already been processed" });
+    }
+    request.status = "rejected";
+    await request.save();
+    res.json({ message: "Connection request rejected" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
--- api/routes/connections.route.js
+++ api/routes/connections.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { protectRoute } from '../middleware/auth.middleware.js';
-import { acceptConnectionRequest, sendConnectionRequest } from '../controllers/connection.controller.js';
+import { acceptConnectionRequest, rejectConnectionRequest, sendConnectionRequest } from '../controllers/connection.controller.js';
 
 const router = express.Router()
 
 router.post('/request/:userId',protectRoute,sendConnectionRequest);
 router.put("/accept/:requestId", protectRoute, acceptConnectionRequest);
+router.put('/reject/:requestId',protectRoute,rejectConnectionRequest)
 
 export default router;
