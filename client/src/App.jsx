bbd55f60e531ff42c3079b1f0e23a66b9c9870e5
vikram17dp <vikramdp505@gmail.com>
2024-09-24 12:44:20 +0530

    Routes setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import Layout from './components/Layout'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import SignupPage from './pages/auth/SignupPage'
+import LoginPage from './pages/auth/LoginPage'
 
 export default function App() {
   return (
-    <div className='text-red-700'>App</div>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/signup' element={<SignupPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+      </Routes>
+    </Layout>
   )
 }
---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+export default function Layout({children}) {
+  return (
+    <div className='min-h-screen bg-base-100'>
+        <Navbar/>
+        <main className='max-w-7xl mx-auto px-4 py-6'>
+        {children}
+        </main>
+    </div>
+  )
+}
