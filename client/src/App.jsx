9f5f1dc29ecef289a079e9145cc03c528c28edfa
vikram17dp <vikramdp505@gmail.com>
2024-09-24 13:11:06 +0530

    small changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Layout from './components/Layout'
+import Layout from './components/Layout/Layout'
 import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import SignupPage from './pages/auth/SignupPage'
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -16,7 +16,7 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
 
       <App />
     </QueryClientProvider>
