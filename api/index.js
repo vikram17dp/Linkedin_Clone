21949678589844a844158567e912bcda7948417f
vikram17dp <vikramdp505@gmail.com>
2024-09-24 11:56:22 +0530

    setting up the limit 5mb to the files

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ dotnev.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({limit:"5mb"}));
 app.use(cookieParser());
 
 app.use('/api/v1/auth',authRoutes)
