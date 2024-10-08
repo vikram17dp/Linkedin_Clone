558d14a44221bde40c7a06f0cb1a7fe437325378
vikram17dp <vikramdp505@gmail.com>
2024-10-08 23:02:41 +0530

    small changes in the axios

--- client/src/lib/axois.js
+++ client/src/lib/axois.js
@@ -1,7 +1,7 @@
 import axios  from "axios";
 
  const axiosInstance = axios.create({
-    baseURL:"http://localhost:3000/api/v1",
+    baseURL:import.meta.mode === "development" ? "http://localhost:3000/api/v1" : "/api/v1",
     withCredentials:true
 })
 axiosInstance.interceptors.request.use(
