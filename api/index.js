2ba0d3d55b6ec3337434ff6dcd1b015f0297e725
vikram17dp <vikramdp505@gmail.com>
2024-10-08 22:46:51 +0530

    building the client foalder

--- api/index.js
+++ api/index.js
@@ -10,10 +10,16 @@ import connectionRoute from './routes/connections.route.js'
 import cors from 'cors'
 import cookieParser from "cookie-parser";
 import path from 'path';
+import { fileURLToPath } from 'url';
+
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 
 app.use(express.json({limit:"5mb"}));
 app.use(cookieParser());
@@ -21,11 +27,10 @@ app.use(cors({
   origin:"http://localhost:5173",
   credentials:true
 }))
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
 app.use('/api/v1/auth',authRoutes)
