7dec9cbf4931fcdbbe6dfa8607f3b4d02e3e0fdc
vikram17dp <vikramdp505@gmail.com>
2024-10-09 17:06:24 +0530

    last changes realted to the dist

--- api/index.js
+++ api/index.js
@@ -27,9 +27,9 @@ app.use(cors({
   origin: "http://localhost:5173",
   credentials:true
 }))
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '/client/dist', 'index.html'));
 });
 
 
