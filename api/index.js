4ee6231783be5f6849c4669e6686b78039cd1d74
vikram17dp <vikramdp505@gmail.com>
2024-10-09 17:20:59 +0530

    some changes realted to the  production

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-
 import authRoutes from './routes/auth.route.js'
 import userRoutes from './routes/user.route.js'
 import postRoutes from './routes/post.route.js'
@@ -12,35 +11,44 @@ import cookieParser from "cookie-parser";
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
-
 app.use(express.json({limit:"5mb"}));
 app.use(cookieParser());
+
+const allowedOrigins = ['https://linkedin-clone-2.onrender.com', 'http://localhost:5173'];
+
 app.use(cors({
-  origin: "http://localhost:5173",
-  credentials:true
-}))
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true
+}));
+
+
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/posts', postRoutes);
+app.use('/api/v1/notifications', notificationRoutes);
+app.use('/api/v1/connection', connectionRoute);
+
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/client/dist")));
+  const clientPath = path.resolve(__dirname, '..', 'client', 'dist');
+  app.use(express.static(clientPath));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-	});
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(clientPath, 'index.html'));
+  });
 }
-
-
-app.use('/api/v1/auth',authRoutes)
-app.use('/api/v1/users',userRoutes)
-app.use('/api/v1/posts',postRoutes)
-app.use('/api/v1/notifications',notificationRoutes)
-app.use('/api/v1/connection',connectionRoute)
 
 app.listen(PORT, () => {
   console.log(`The server is listening on ${PORT}`);
@@ -49,7 +57,7 @@ app.listen(PORT, () => {
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("mongoose  is connected!");
+    console.log("mongoose is connected!");
   })
   .catch((err) => {
     console.log(err);
