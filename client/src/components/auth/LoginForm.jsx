ef43dcd132b6571acddf7dc36aa0309ec28f75ee
vikram17dp <vikramdp505@gmail.com>
2024-11-12 16:24:14 +0530

    adding some padding in the login page

--- client/src/components/auth/LoginForm.jsx
+++ client/src/components/auth/LoginForm.jsx
@@ -3,7 +3,7 @@ import { axiosInstance } from "../../lib/axois.js";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { Loader } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function LoginForm() {
   const [username, setUsername] = useState("");
@@ -27,6 +27,9 @@ export default function LoginForm() {
     e.preventDefault();
     loginMutation({ username, password });
   };
+  const handleJoinNow = () => {
+    navigate("/signup");
+  };
 
   return (
     <form onSubmit={handleSubmit} className="space-y-4">
@@ -49,6 +52,24 @@ export default function LoginForm() {
       <button type="submit" className="btn btn-primary w-full">
         {isLoading ? <Loader className="size-5 animate-spin" /> : "Login"}
       </button>
+      <div className="mt-6">
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <div className="w-full border-t border-gray-300"></div>
+              </div>
+              <div className="relative flex justify-center text-sm">
+                <span className="px-2 bg-white text-gray-500">
+                  New to CarrerLink?
+                </span>
+              </div>
+            </div>
+
+            
+      <button type="button" onClick={handleJoinNow} className=" btn-primary w-full">
+      <Link to="/login" className='w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-black-600 bg-gray-600 mt-5 '>
+      join now</Link>
+      </button>
+          </div>
      
     </form>
   );
